1efea19622bfb85b066e4afcdd18e42a807c886d
Senuri Mallikarachchi <senurimallikarachchi@gmail.com>
2025-10-09 22:35:19 +0530

    Implement frontend for event based audio switching

---
+++ app/src/main/java/com/example/silentmate/AddEventActivity.kt
@@ -0,0 +1,14 @@
+package com.example.silentmate
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_event)
+    }
+}
---
+++ app/src/main/java/com/example/silentmate/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.silentmate
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.silentmate.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.addEventFab.setOnClickListener {
+            val intent = Intent(this, AddEventActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
